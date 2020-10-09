208858fe80874b627bb80d7203d3338f5f5dda82
Vasyl Semak <vasylsmk1401@gmail.com>
2020-10-09 11:28:10 -0400

    create posts route

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ app.use(morgan('dev'));
 // Routes
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get('/posts', (req, res) => {
+  const html = `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Wizard News</title>
+    </head>
+    <body>
+      <ul>
+          ${list()
+            .map(post => `<li>${post.title}. Author - ${post.name}</li>`)
+            .join('')}
+      </ul>
+    </body>
+    </html>
+  `;
+  res.send(html);
+});
+
 const PORT = 1337;
 
 app.listen(PORT, () => {
